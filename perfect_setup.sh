4499110175b6cdcc21a2490056d15b68db809e84
Zack Philipps <zack@tribeswell.com>
2015-06-17 14:36:28 -0400

    Eliminate error message.

--- perfect_setup.sh
+++ perfect_setup.sh
@@ -5,11 +5,9 @@ printf "What is your BitBucket username? "
 read BBUSERNAME
 printf "What is the name of the BitBucket repo (i.e. perfect-setup or example-site)? "
 read BBREPO
-printf "What should the directory be called on your system (should be another BitBucket repo name, default $BBREPO)? "
+printf "What should the directory be called on your system? This should be another BitBucket repo name. Press return/enter for the default. (default $BBREPO)? "
 read NEWDIR
-if [ "$NEWDIR" = "" ]; then
-	NEWDIR="$BBREPO"
-fi
+NEWDIR=${NEWDIR:-$BBREPO}
 git clone https://$BBUSERNAME@bitbucket.org/tribeswell-llc/$BBREPO.git $NEWDIR
 cd $NEWDIR
 git remote set-url origin https://$BBUSERNAME@bitbucket.org/tribeswell-llc/$BBREPO.git
@@ -37,9 +35,9 @@ WPHOME=$"wp_home_here"
 DBUSER=$"username_here"
 DBPASS=$"password_here"
 DBNAME=$"database_name_here"
-sed -i '' -e "s/${WPHOME}/http:\/\/${NEWDIR}:7888/g" ./wp-config.php
-sed -i '' -e "s/${DBUSER}/${NEWDB}/g" ./wp-config.php
-sed -i '' -e "s/${DBPASS}/${MYSQLPWD}/g" ./wp-config.php
-sed -i '' -e "s/${DBNAME}/${NEWDB}/g" ./wp-config.php
+sed -i'' -e "s/${WPHOME}/http:\/\/${NEWDIR}:7888/g" ./wp-config.php
+sed -i'' -e "s/${DBUSER}/${NEWDB}/g" ./wp-config.php
+sed -i'' -e "s/${DBPASS}/${MYSQLPWD}/g" ./wp-config.php
+sed -i'' -e "s/${DBNAME}/${NEWDB}/g" ./wp-config.php
 echo "" && echo "Success!" && open http://$NEWDIR:7888/wp/wp-admin/
 fi
