4c438a25f724a6456cee2208dadec522ade610c2
Zack Philipps <zack@tribeswell.com>
2015-06-19 19:31:14 -0400

    Revert to old dir structure.

--- perfect_setup.sh
+++ perfect_setup.sh
@@ -22,30 +22,26 @@ read -s MYSQLPWD
 if [[ "$OSTYPE" == "msys" ]]; then
 	echo ""
 	/c/MAMP/bin/mysql/bin/mysql --host=localhost -uroot -proot -e "CREATE USER '$NEWDB'@'localhost' IDENTIFIED BY '$MYSQLPWD'; CREATE DATABASE $NEWDB; GRANT ALL ON $NEWDB.* TO '$NEWDB'@'localhost';"
-
 	/c/MAMP/bin/mysql/bin/mysql --host=localhost -u$NEWDB -p$MYSQLPWD $NEWDB < ./perfect_setup.sql
 else
 	echo ""
 	/Applications/MAMP/Library/bin/mysql --host=localhost -uroot -proot -e "CREATE USER '$NEWDB'@'localhost' IDENTIFIED BY '$MYSQLPWD'; CREATE DATABASE $NEWDB; GRANT ALL ON $NEWDB.* TO '$NEWDB'@'localhost';"
-
 	/Applications/MAMP/Library/bin/mysql --host=localhost -u$NEWDB -p$MYSQLPWD $NEWDB < ./perfect_setup.sql
 fi
 
 if [ -f ./wp-config.php ]
 then
-	echo "" && echo "There appears to be a wp-config.php file already." && open http://$NEWDIR:7888/wp/wp-admin/install.php
+	echo "" && echo "There appears to be a wp-config.php file already." && open http://$NEWDIR:7888/wp-admin/install.php
 else
 	cp -n ./wp-config-sample.php ./wp-config.php
 	SECRETKEYS=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
 	EXISTINGKEYS='put your unique phrase here'
 	printf '%s\n' "g/$EXISTINGKEYS/d" a "$SECRETKEYS" . w | ed -s ./wp-config.php
-	WPHOME=$"wp_home_here"
 	DBUSER=$"username_here"
 	DBPASS=$"password_here"
 	DBNAME=$"database_name_here"
-	sed -i '' -e "s/${WPHOME}/http:\/\/${NEWDIR}:7888/g" ./wp-config.php
 	sed -i '' -e "s/${DBUSER}/${NEWDB}/g" ./wp-config.php
 	sed -i '' -e "s/${DBPASS}/${MYSQLPWD}/g" ./wp-config.php
 	sed -i '' -e "s/${DBNAME}/${NEWDB}/g" ./wp-config.php
-	echo "" && echo "Success!" && open http://$NEWDIR:7888/wp/wp-admin/
+	echo "" && echo "Success!" && open http://$NEWDIR:7888/wp-admin/
 fi
