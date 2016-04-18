998d125861be050c77ee8d936650e88f1e07af6f
Zack Philipps <zack@tribeswell.com>
2016-04-18 14:06:29 -0400

    Just use the right one all the dang time.

--- perfect_setup.sh
+++ perfect_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# perfect_setup.sh v0.0.6
+# perfect_setup.sh v0.0.7
 
 clear
 cd ~/Sites
@@ -35,7 +35,7 @@ if [ -f ./wp-config.php ]
 then
 	echo "" && echo "There appears to be a wp-config.php file already." && open http://$NEWDIR:7888/wp-admin/install.php
 else
-	cp -n ./wp-config-sample.php ./wp-config.php
+	cp -n ./_wp-config-sample.php ./wp-config.php
 	SECRETKEYS=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
 	EXISTINGKEYS='put your unique phrase here'
 	printf '%s\n' "g/$EXISTINGKEYS/d" a "$SECRETKEYS" . w | ed -s ./wp-config.php
